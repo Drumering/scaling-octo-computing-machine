3677a1f68a45dc26d057850009442cf6297a21de
Anderson Mendes de Almeida <almeida.anderson.mendes@gmail.com>
2025-06-12 21:00:09 -0300

    Add Account and Event interfaces along with AccountRepository interface

---
+++ src/domain/entities/Event.ts
@@ -0,0 +1,8 @@
+import { EventType } from "../../shared/types/EventType";
+
+export interface Event {
+    type: EventType
+    origin?: string
+    destination?: string
+    amount: number
+}
---
+++ src/domain/ports/AccountRepository.ts
@@ -0,0 +1,10 @@
+import { Account } from "../entities/Account";
+
+export interface AccountRepository {
+    findAll(): Promise<Account[]>
+    findById(id: string): Promise<Account | null>
+    create(account: Account): Promise<Account>
+    update(updateAccount: Account): Promise<Account>
+    delete(id: string): Promise<void>
+    reset(): Promise<void>
+}
